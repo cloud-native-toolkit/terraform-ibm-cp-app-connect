d551a94fc5b46265e4a841fabe2edf08c6d16655
Sean Sundberg <seansund@us.ibm.com>
2021-02-05 09:13:46 -0600

    Updates wait for crd logic

--- scripts/wait-for-crds.sh
+++ scripts/wait-for-crds.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-CRD_LIST="designerauthoring.appconnect.ibm.com,integrationserver.appconnect.ibm.com,integrationserver.appconnect.ibm.com,dashboard.appconnect.ibm.com"
+CRD_LIST="designerauthorings.appconnect.ibm.com,integrationservers.appconnect.ibm.com,integrationservers.appconnect.ibm.com,dashboards.appconnect.ibm.com"
 
 IFS=',' read -ra CRDS <<< "$CRD_LIST"
 for crd in "${CRDS[@]}"; do
   retrycount=10
-  while ! k get "${crd}" -A 1 > /dev/null 2> /dev/null && [[ "${retrycount}" -gt 0 ]]; do
+  while [[ $(k get crd "${crd}" -o jsonpath='{.metadata.name}{"\n"}' | wc -l) -eq 0 ]] && [[ "${retrycount}" -gt 0 ]]; do
     echo "Waiting for crd: ${crd}"
+    retrycount=$(( retrycount - 1 ))
     sleep 30
   done
 
-  if ! k get "${crd}" -A 1 > /dev/null 2> /dev/null; then
+  if [[ $(k get crd "${crd}" -o jsonpath='{.metadata.name}{"\n"}' | wc -l) -eq 0 ]]; then
     echo "Timed out waiting for crd: ${crd}"
     exit 1
   fi
