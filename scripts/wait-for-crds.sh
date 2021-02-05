144bd4c6e40b88ec0bcda4d5110f34746223cf02
Sean Sundberg <seansund@us.ibm.com>
2021-02-05 09:25:01 -0600

    Fixes typo in wait-for-crds

--- scripts/wait-for-crds.sh
+++ scripts/wait-for-crds.sh
@@ -5,13 +5,13 @@ CRD_LIST="designerauthorings.appconnect.ibm.com,integrationservers.appconnect.ib
 IFS=',' read -ra CRDS <<< "$CRD_LIST"
 for crd in "${CRDS[@]}"; do
   retrycount=10
-  while [[ $(k get crd "${crd}" -o jsonpath='{.metadata.name}{"\n"}' | wc -l) -eq 0 ]] && [[ "${retrycount}" -gt 0 ]]; do
+  while [[ $(kubectl get crd "${crd}" -o jsonpath='{.metadata.name}{"\n"}' | wc -l) -eq 0 ]] && [[ "${retrycount}" -gt 0 ]]; do
     echo "Waiting for crd: ${crd}"
     retrycount=$(( retrycount - 1 ))
     sleep 30
   done
 
-  if [[ $(k get crd "${crd}" -o jsonpath='{.metadata.name}{"\n"}' | wc -l) -eq 0 ]]; then
+  if [[ $(kubectl get crd "${crd}" -o jsonpath='{.metadata.name}{"\n"}' | wc -l) -eq 0 ]]; then
     echo "Timed out waiting for crd: ${crd}"
     exit 1
   fi
