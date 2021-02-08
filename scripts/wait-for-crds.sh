4223e0211aac7e6bd39e305e56fb5ef18e0e2ab8
Sean Sundberg <seansund@us.ibm.com>
2021-02-08 07:41:36 -0600

    Initial version (#1)
    * Looks up default storage class if one is not provided
    * Provisions the operator and SwitchServer, IntegrationServer, Designer, and Dashboard instances
    * Allows Dashboard to be conditionally installed (it requires file storage)

---
+++ scripts/wait-for-crds.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+CRD_LIST="designerauthorings.appconnect.ibm.com,integrationservers.appconnect.ibm.com,integrationservers.appconnect.ibm.com,dashboards.appconnect.ibm.com"
+
+IFS=',' read -ra CRDS <<< "$CRD_LIST"
+for crd in "${CRDS[@]}"; do
+  retrycount=10
+  while [[ $(kubectl get crd "${crd}" -o jsonpath='{.metadata.name}{"\n"}' | wc -l) -eq 0 ]] && [[ "${retrycount}" -gt 0 ]]; do
+    echo "Waiting for crd: ${crd}"
+    retrycount=$(( retrycount - 1 ))
+    sleep 30
+  done
+
+  if [[ $(kubectl get crd "${crd}" -o jsonpath='{.metadata.name}{"\n"}' | wc -l) -eq 0 ]]; then
+    echo "Timed out waiting for crd: ${crd}"
+    exit 1
+  fi
+done
